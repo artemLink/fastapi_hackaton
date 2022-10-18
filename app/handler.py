ffed66a39f12f9dcce26a86fcd1f1b2e1e2427f4
artemLink <artemLink>
2022-10-18 16:54:53 +0300

    Change algorithm and url
    Signed-off-by: artemLink <artemLink>

--- app/handler.py
+++ app/handler.py
@@ -1,33 +1,60 @@
 from fastapi import APIRouter, Body, Depends
+from sqlalchemy import func
 from app.forms import book_of_references
 from app.models import connect_db, references_table, Base, news_table, gum_help_table
 
 router = APIRouter()
 
 
-@router.get('/take_references/{pk}')
-def index(pk: int, database=Depends(connect_db)):
-    req = database.query(references_table).filter(references_table.id == pk).one_or_none()
+@router.post('api/GetReferences')
+def references(database=Depends(connect_db)):
+    book = {'content': []}
+    req = database.query(references_table).all()
+    for item in req:
+        book['content'].append({'title': item.title,
+                                'descript': item.descript.split(';')})
 
-    return {'title': req.title,
-            'descript': req.descript}
-
-
-@router.get('/take_news/{pk}')
-def index(pk: int, database=Depends(connect_db)):
-    req = database.query(news_table).filter(news_table.id == pk).one_or_none()
-
-    return {'title': req.title,
-            'descript': req.descript,
-            'region': req.region}
+    return book
 
 
-@router.get('/take_gum/{pk}')
-def gum_help(pk: int, database=Depends(connect_db)):
-    req = database.query(gum_help_table).filter(gum_help_table.id == pk).one_or_none()
+@router.get('api/GetNews/{id_from}')
+def news(id_form: int, database=Depends(connect_db)):
+    book = {'content': []}
+    count_of_news = database.query(news_table).order_by(news_table.id.desc()).first()
+    if count_of_news.id - id_form >= 1:
+        req = database.query(news_table).filter(news_table.id > id_form).all()
+        for item in req:
+            book['content'].append(
+                {'title': item.title,
+                 'descript': item.descript,
+                 'region': item.region}
+            )
+    elif count_of_news.id == 0:
+        req = database.query(news_table).all()
+        for item in req:
+            book['content'].append(
+                {'title': item.title,
+                 'descript': item.descript,
+                 'region': item.region}
+            )
+    else:
+        return {'content': []}
 
-    return {
-        'title': req.title,
-        'address': req.address,
-        'timing': req.timing
-    }
+    return book
+
+
+@router.get('api/GetGum/{city}')
+def gum_help(city: str, database=Depends(connect_db)):
+    book = {'content': []}
+    items = database.query(gum_help_table).filter(gum_help_table.city == city).all()
+
+    for req in items:
+        book['content'].append(
+            {
+                'title': req.title,
+                'address': req.address,
+                'timing': req.timing
+            }
+        )
+
+    return book
